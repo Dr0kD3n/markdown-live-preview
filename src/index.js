be0dbdcac41ea345d5a2e6e22f93b7f5c8f0f23e
Dr0kD3n <bkerikbk@gmail.com>
2018-08-25 19:23:56 +0300

    25/08

--- src/index.js
+++ src/index.js
@@ -1,56 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React,{ Component } from 'react';
+import { render } from 'react-dom';
 import './index.css';
 import Remarkable from 'remarkable';    
 
-class MarkdownEditor extends React.Component {
+class MarkdownEditor extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
         this.state = { 
-            value: 'Type some *markdown* here!'
+            value: '# Your markdown here!'
         };
     }
-
-
     handleChange(e) {
         this.setState({ 
             value: e.target.value
         });
     }
-
     getRawMarkup() {
         const md = new Remarkable();
         return {__html: md.render(this.state.value)};
     }
-
     render() {
         return (
             <div className="container">
-                <div className="input">
-                    <h3>Input</h3>
-                <textarea
-                    className="input-text"
-                    onChange={this.handleChange}
-                    defaultValue={this.state.value}
-                />
-                </div>
-                <div className="output">
-                    <h3>Markdown</h3>
-                <div 
-                    dangerouslySetInnerHTML={this.getRawMarkup()}
-                    className="output-text"
-                >
-                </div>
-                </div>                
+            <div className="input">
+            <h3>Input</h3>
+
+            <textarea
+            className="input-text"
+            onChange={this.handleChange}
+            defaultValue={this.state.value} 
+            />
+
             </div>
-        )
+            <div className="output">
+            <h3>Output</h3>
+
+            <div 
+            dangerouslySetInnerHTML={this.getRawMarkup()}
+            className="output-text" >
+
+            </div>
+            </div>                
+            </div>
+            )
     }
 }
-
-// ========================================
-
-ReactDOM.render(
-    <MarkdownEditor />,
-    document.getElementById('root')
-);
+render(<MarkdownEditor />, document.getElementById('root'));
